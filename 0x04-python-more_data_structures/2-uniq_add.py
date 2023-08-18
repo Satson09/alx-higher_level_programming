78e1fc38b9a69f103f364b1b521f580e24170c46
satson09 <satsunjohnson@gmail.com>
2023-08-18 14:12:06 +0100

    python data structures

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
+    """
+    A function that adds all unique
+    integers in a list (only once for each integer)
+    """
     setlist = set(my_list)
     x = 0
     for i in setlist:
